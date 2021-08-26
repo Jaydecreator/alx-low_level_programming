2905e9cc19c54925bb1ff856f915bd4375118077
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2021-08-26 18:18:09 +0000

    fixed errorC programm written to print with use of function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,16 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}#include <stdio.h>
-
-/**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
