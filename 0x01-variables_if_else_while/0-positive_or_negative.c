62090f9f572743bd27054f4414495e67e2faaacf
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2021-08-27 18:52:52 +0000

    varibles files if while do while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - it will assign random number to (n)
+ * condition1 - if n > 0 = is_positive
+ * condition2 - if n == 0 = is_zero
+ * condition3 - if n < 0 = is_negative
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
