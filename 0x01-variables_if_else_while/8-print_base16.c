4c06024cd1d9ca41844993c46ceee1f5be722044
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2021-08-27 19:44:13 +0000

    fixed again

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point print all the nubers of base 16 in lowercase
+ * main - Entry point
  * Return:0
  */
 int main(void)
 {
 	int i, y;
 	for (i = 0; i < 10; i++)
-
 	{
 		putchar (i + '0');
 	}
