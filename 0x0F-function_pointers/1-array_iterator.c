6092a8a9e5c290f57247d7dcc309059917c400ee
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2021-09-30 15:17:30 +0000

    fixed done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
+	unsigned int i;
 
-if (array != NULL && action != NULL)
-{
+	if (!array || !action)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
